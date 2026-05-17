052a3e07bc88504bcd445ff83498ffdaa8a5bf5f
Hanibal1963 <18749753+Hanibal1963@users.noreply.github.com>
2026-05-17 11:44:09 +0200

    Erweiterung der NotifyForm-Konfiguration im UI
    Neue Steuerelemente zur Konfiguration von Titel, Nachricht, Anzeigedauer, Stil und Design der NotifyForm hinzugefügt. Konstruktor und Button-Click-Handler in `FormMain.vb` implementiert, um die NotifyForm dynamisch zu aktualisieren und anzuzeigen.
    
    Assembly-Attribute in `AssemblyInfo.vb` aktualisiert, darunter Versionsnummer und Namespace-Anpassungen. Projektdatei `NotifyFormControlTest.vbproj` entsprechend angepasst.

--- NotifyFormControlTest/FormMain.vb
+++ NotifyFormControlTest/FormMain.vb
@@ -11,7 +11,20 @@ Public Class FormMain
         ' Dieser Aufruf ist für den Designer erforderlich.
         Me.InitializeComponent()
         ' Fügen Sie Initialisierungen nach dem InitializeComponent()-Aufruf hinzu.
+        Me.TextBox_Title.Text = Me.NotifyForm.Title
+        Me.TextBox_Message.Text = Me.NotifyForm.Message
+        Me.NumericUpDown_ShowTime.Value = CDec(Me.NotifyForm.ShowTime / 1000)
+        Me.ComboBox_Style.SelectedIndex = CInt(Me.NotifyForm.Style)
+        Me.ComboBox_Design.SelectedIndex = CInt(Me.NotifyForm.Design)
+    End Sub
 
+    Private Sub Button_ShowWindow_Click(sender As Object, e As EventArgs) Handles Button_ShowWindow.Click
+        Me.NotifyForm.Title = Me.TextBox_Title.Text
+        Me.NotifyForm.Message = Me.TextBox_Message.Text
+        Me.NotifyForm.ShowTime = CInt(Me.NumericUpDown_ShowTime.Value * 1000)
+        Me.NotifyForm.Style = CType(Me.ComboBox_Style.SelectedIndex, SchlumpfSoft.Controls.NotifyFormControl.NotifyFormStyle)
+        Me.NotifyForm.Design = CType(Me.ComboBox_Design.SelectedIndex, SchlumpfSoft.Controls.NotifyFormControl.NotifyFormDesign)
+        Me.NotifyForm.Show()
     End Sub
 
 End Class
--- NotifyFormControlTest/My Project/AssemblyInfo.vb
+++ NotifyFormControlTest/My Project/AssemblyInfo.vb
@@ -1,34 +1,16 @@
-Imports System.Resources
-Imports System
-Imports System.Reflection
-Imports System.Runtime.InteropServices
+' --------------------------------------------------------------------------------------------------------
+' Datei: AssemblyInfo.vb
+' Author: Andreas Sauer
+' Datum: 17.05.2026
+' --------------------------------------------------------------------------------------------------------
 
-' Allgemeine Informationen über eine Assembly werden über die folgenden
-' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
-' die einer Assembly zugeordnet sind.
-
-' Werte der Assemblyattribute überprüfen
-
-<Assembly: AssemblyTitle("NotifyFormControlTest")>
-<Assembly: AssemblyDescription("Testanwendung für NotifyFormControl")>
-<Assembly: AssemblyCompany("privat")>
-<Assembly: AssemblyProduct("NotifyFormControlTest")>
-<Assembly: AssemblyCopyright("Copyright © 2026 by Andreas Sauer")>
-<Assembly: AssemblyTrademark("SchlumpfSoft")>
-
-<Assembly: ComVisible(True)>
-
-'Die folgende GUID wird für die typelib-ID verwendet, wenn dieses Projekt für COM verfügbar gemacht wird.
-<Assembly: Guid("5bd392d4-0cbc-4336-aa77-09df3e03944c")>
-
-' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
-'
-'      Hauptversion
-'      Nebenversion
-'      Buildnummer
-'      Revision
-'
-
-<Assembly: AssemblyVersion("1.2026.0507.1")>
-<Assembly: AssemblyFileVersion("1.2026.0507.1")>
-<Assembly: NeutralResourcesLanguage("de-DE")>
+<Assembly: Reflection.AssemblyTitle("NotifyFormControlTest")>
+<Assembly: Reflection.AssemblyDescription("Testanwendung für NotifyFormControl")>
+<Assembly: Reflection.AssemblyCompany("privat")>
+<Assembly: Reflection.AssemblyProduct("NotifyFormControlTest")>
+<Assembly: Reflection.AssemblyCopyright("Copyright © 2026 by Andreas Sauer")>
+<Assembly: Reflection.AssemblyTrademark("SchlumpfSoft")>
+<Assembly: Runtime.InteropServices.ComVisible(False)>
+<Assembly: Reflection.AssemblyVersion("1.2026.0517.0")>
+<Assembly: Reflection.AssemblyFileVersion("1.2026.0517.0")>
+<Assembly: Resources.NeutralResourcesLanguage("de-DE")>
